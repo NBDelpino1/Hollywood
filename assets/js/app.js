a6b680e2f1b2b84c30287f8d7b12008f5e4a0d2c
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-04 14:44:01 -0500

    did not work reset to original

--- assets/js/app.js
+++ assets/js/app.js
@@ -233,30 +233,27 @@ var game = {
 
 
 
-
-// //============= This works animating the background image but the image sliudes out of biew and doesnt com e back===============
+//
+// // //============= This works animating the background image but the image sliudes out of biew and doesnt com e back===============
 // speed in milliseconds
-var scrollSpeed = 100;
-
-// set the default position
-var current = 0;
-
-// set the direction
-var direction = 'h';
-
-function bgscroll() {
-
-    // 1 pixel row at a time
-    current -= 1;
-
-    // move the background with backgrond-position css properties
-    $('.main-wrapper').css("backgroundPosition", (direction == 'h') ? current + "px 0" : "0 " + current + "px");
-
-}
-//Calls the scrolling function repeatedly
-setInterval("bgscroll()", scrollSpeed);
+// var scrollSpeed = 70;
+//
+// // set the default position
+// var current = 0;
+//
+// // set the direction
+// var direction = 'h';
+//
+// function bgscroll() {
+//
+//     // 1 pixel row at a time
+//     current -= 1;
+//
+//     // move the background with backgrond-position css properties
+//     $('.main-wrapper').css("backgroundPosition", (direction == 'h') ? current + "px 0" : "0 " + current + "px");
+//
+// }
+// //Calls the scrolling function repeatedly
+// setInterval("bgscroll()", scrollSpeed);
 // //============= This works animating the background image but the image sliudes out of biew and doesnt com e back===============
-
-
-
 
