97daffa68489b326e0a641f83c4865cf4dab32d2
Nicholas Delpino <NBDelpino4@gmail.com>
2017-03-04 00:35:51 -0500

    finished styling results, if time permits can refine

--- assets/js/app.js
+++ assets/js/app.js
@@ -87,7 +87,7 @@ var game = {
     correct:0,
     // to keep track of how many incorrect answers
     incorrect:0,
-// to keep track of unanswered questions
+    // to keep track of unanswered questions
     unanswered: 0,
     // incharge of changing the timer
     countdown: function () {
@@ -99,24 +99,30 @@ var game = {
         }
     },
 
+
     // set the timer to start decreasing as the question loads
     // post current question to the page
+
+
     loadQuestion: function () {
 
         timer = setInterval(game.countdown,1000);
         $('#subwrapper').html('<h2 class="timer">Time Remaining: <span id="counter">30</span><span> sec</span></h2>');
-        (function blink() {
-            $('.timer').fadeOut(515).fadeIn(515, blink);
-        })();
         $('#subwrapper').append('<h3 class="display-question">' + questions[game.currentQuestion].question + '</h3>');
         for(var i = 0; i < questions[game.currentQuestion].answers.length; i ++) {
             $('#subwrapper').append('<button class="btn btn-lg btn-default answer-button" id="button-' + i + '" data-name="' + questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>')
         }
+        (function blink() {
+            $('.timer').fadeOut(515).fadeIn(515, blink);
+        })();
     },
+
 
     // set counter back to origin to there is a fresh 30 seconds to answer the new question
     //update html with new time
     // load next question (make sure not to load same question)
+
+
     nextQuestion: function () {
 
         game.counter = 30;
@@ -125,16 +131,19 @@ var game = {
         game.loadQuestion();
     },
 
+
     // stop timer so it doesn't start going into negative
     // increase the amt of unanswered questions
     // alert user that they have run out of time
     // let user know what the correct answer was
     // if final question go to the results screen if not go to the end
+
+
     timeUp: function () {
         clearInterval(timer);
         game.unanswered ++;
-        $('#subwrapper').html('<h2 class="display-out-of-time">OUT OF TIME!</h2>');
-        $('#subwrapper').append('<h3>The correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        $('#subwrapper').html('<h2>OUT OF TIME!</h2>');
+        $('#subwrapper').append('<h3>Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
         if(game.currentQuestion == questions.length -1) {
             setTimeout(game.results, 1 * 1000);
         } else {
@@ -142,18 +151,22 @@ var game = {
         }
     },
 
+
     // load after results after last question
     // clear timer
     // advise user they are done and what their score was
     // append button so user can restart game if they choose
+
+
     results: function () {
         clearInterval(timer);
-        $('#subwrapper').html('<h2>ALL DONE!</h2>');
-        $('#subwrapper').append('<h3>CORRECT: </h3>' + game.correct);
-        $('#subwrapper').append('<h3>INCORRECT: </h3>' + game.incorrect);
-        $('#subwrapper').append('<h3>UNANSWERED: </h3>' + game.unanswered);
-        $('#subwrapper').append('<button id="reset">RESTART GAME</button>');
+        $('#subwrapper').html('<h2>GAME OVER!</h2>');
+        $('#subwrapper').append('<h3>Correct: ' + game.correct + '</h3>');
+        $('#subwrapper').append('<h3>Incorrect: ' + game.incorrect + '</h3>');
+        $('#subwrapper').append('<h3>Unanswered: ' + game.unanswered + '</h3>');
+        $('#subwrapper').append('<button class="btn btn-lg btn-default reset-button" id="reset">Restart Game</button>');
     },
+
 
     // clear interval / stop timer after button clicked
     // pass in what is being clicked, compare it to the correct answer and do something
@@ -167,30 +180,19 @@ var game = {
         }
     },
 
+
     // test to make sure data was passed and comparison ok
     // clear timer
     // increase number of correct answers
     // advise user they got it correct
     // add something that either takes user to the results screen if game over or move to the next question
+
+
     answeredCorrectly: function () {
         console.log('YOU GOT IT!');
         clearInterval(timer);
         game.correct ++;
-        $('#subwrapper').html('<h2>YOU GOT IT CORRECT!</h2>');
-        if(game.currentQuestion == questions.length -1) {
-            setTimeout(game.results, 1 * 1000);
-        } else {
-            setTimeout(game.nextQuestion, 1 * 1000)
-        }
-    },
-    // can use the same logic as answered correctly just edit variable name and statement
-    // let user know what the correct answer was
-    answeredIncorrectly: function () {
-        console.log('WRONG :(');
-        clearInterval(timer);
-        game.incorrect ++;
-        $('#subwrapper').html('<h2>YOU GOT IT WRONG!</h2>');
-        $('#subwrapper').append('<h3>THE CORRECT ANSWER WAS: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        $('#subwrapper').html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span><h2>YOU GOT IT CORRECT!</h2>');
         if(game.currentQuestion == questions.length -1) {
             setTimeout(game.results, 1 * 1000);
         } else {
@@ -198,7 +200,28 @@ var game = {
         }
     },
 
+
+    // can use the same logic as answered correctly just edit variable name and statement
+    // let user know what the correct answer was
+
+
+    answeredIncorrectly: function () {
+        console.log('WRONG!');
+        clearInterval(timer);
+        game.incorrect ++;
+        $('#subwrapper').html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span><h2>OOPS INCORRECT</h2>');
+        $('#subwrapper').append('<h3>Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if(game.currentQuestion == questions.length -1) {
+            setTimeout(game.results, 1 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 1 * 1000)
+        }
+    },
+
+
     // set everything back to the original amounts when user restarts game
+
+
     reset: function () {
         game.currentQuestion = 0;
         game.counter = 0;
